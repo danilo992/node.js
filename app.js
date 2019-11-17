0c938fdea78d5436f941ae4416bb5f108eebf421
danilo992 <tadeudanilo46@gmail.com>
2019-11-17 20:51:00 -0300

    instalação de pacotes via NPM

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Introdução ao node.js");
+let nome = "Danilo";
+let sobreNome = "Tadeu da Silva Conceição ";
+let cor = require("cli-color");
+
+function nomeCompleto(nome, sobreNome) {
+    console.log(cor.yellow("Olá!",  nome, cor.blue(sobreNome)));
+}  
+
+nomeCompleto(nome, sobreNome);
+
