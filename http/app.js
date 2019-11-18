bb27544a89296b4c16586e85d33735afce09cb25
danilo992 <tadeudanilo46@gmail.com>
2019-11-18 13:42:29 -0300

    Retornando arquivos HTML

---
+++ http/app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function(request, response){
+   fs.readFile("index.html", function(erro, conteudo){
+       if(erro){
+           console.log(erro);
+       } else {
+           response.write(conteudo);
+       }
+       response.end();
+   })
+}).listen(8081);
+
+console.log("Servidor rodando em http://localhost:8081");
